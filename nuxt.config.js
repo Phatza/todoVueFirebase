d9c92deab1b97458beca21fb3c8e2f10e984759d
Phatza <b.haphuoc@gmail.com>
2017-07-12 22:50:30 +0200

    delete unuse code

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
+    title: '%s - Todolist application using VueJS 2 & Firebase',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
