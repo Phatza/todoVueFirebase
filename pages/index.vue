f0fce0b8a4a7460a602aeffcb7cd947b54c93b6f
Phatza <b.haphuoc@gmail.com>
2017-07-13 14:54:08 +0200

    index page component

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,24 @@
 <template>
+  <section>
+    <Row type="flex" justify="center" align="middle">
+      <Col span="24">
+        <img src="http://placehold.it/250/250" alt="">
+        <img src="http://placehold.it/250/250" alt="">
+      </Col>
+    </Row>
+    <Row type="flex" justify="center" align="middle">
+      <Col span="24">
+        <h1>Real-time Todolist Using vueJS &amp; Firebase</h1>
+      </Col>
+    </Row>
+    <nuxt-child/>
+  </section>
 </template>
 
 <script>
+  export default {
 
-export default {
-}
+  }
 </script>
 
 <style>
